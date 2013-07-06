2f483aadcc380fe4995a427c2b5b52eef94d547f
Pablo Lagioia <pablo.lagioia@gmail.com>
2013-07-06 17:19:38 -0300

    no message

--- Meat.js
+++ Meat.js
@@ -12,9 +12,17 @@
 		this.mass = 1;
 		this.maxAX = 5;
 		this.maxAY = 5;
+		this.clickedX = 0;
+		this.clickedY = 0;
 	}
 
 	Meat.prototype.onMouseDown = function() {
+	};
+	Meat.prototype.onMouseUp = function() {
+		this.clickedX = null;
+		this.clickedY = null;
+		this.speed.x = -1;
+		this.speed.y = -1;
 	};
 	Meat.prototype.onDrag = function(mouse) {
 
@@ -22,51 +30,15 @@
 			this.clickedX = mouse.x;
 			this.clickedY = mouse.y;
 		}
-	
-		// if ( this.speed.x < 6 && this.speed.y < 6 ) {
-	
-			// var aX = Math.abs(mouse.x - this._x + this.clickedX),
-				// aY = Math.abs(mouse.y - this._y + this.clickedY);
-			
 
-			// var aX = 0, aY = 0;
-			// var aX = 1, aY = 1;
+		this.setLocation(mouse.x + (this._x - this.clickedX), mouse.y + (this._y - this.clickedY));
 
-			// this.speed.x = aX;
-			// this.speed.y = aY;
-			
-			if ( this.speed.x < this.maxAX ) {
-				// this.acceleration.x = aX;
-				this.speed.x += 1;
-			}
-			if ( this.speed.y < this.maxAY ) {
-				// this.acceleration.y = aY;
-				this.speed.y += 1;
-			}
-			
-			if ( !this.direction ) {
-				this.direction = Math2d.getNormalized(Math2d.getVector2d(new Math2d.Vector2d(this._x, this._y), new Math2d.Vector2d(mouse.x, mouse.y)));
-			}
+		this.direction = Math2d.getNormalized(Math2d.getVector2d(new Math2d.Vector2d(this._x, this._y), new Math2d.Vector2d(mouse.x, mouse.y)));
 
-		// }
-		
 	};
 	Meat.prototype.onUpdate = function() {
 
-		// this.speed.x += this.acceleration.x;
-		// this.speed.y += this.acceleration.y;
-		
-		// var aX = this.acceleration.x - this.mass,
-			// aY = this.acceleration.y - this.mass;
-		
-		// if ( aX > 0 ) {
-			// this.acceleration.x = aX;
-		// }
-		// if ( aY > 0 ) {
-			// this.acceleration.y = aY;
-		// }
-		
-		if ( this.direction ) {		
+		if ( this.speed.x && this.speed.y ) {		
 			this.offset(this.direction.x * this.speed.x, this.direction.y * this.speed.y);		
 		}
 		
